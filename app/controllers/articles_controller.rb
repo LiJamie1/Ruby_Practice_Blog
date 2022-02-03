6fb4b28f6dc47fdf1e320bede4744b11d107e547
Jamie <lijamie19828@gmail.com>
2022-02-02 22:21:22 -0800

    new route, view and model validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ApplicationController
+class ArticlesController < ApplicationController  
 
   def index
     @articles = Article.all
@@ -7,4 +7,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
 end
